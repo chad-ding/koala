93c359e49c691e3a12418f46e03cdd62da99fa67
Chad-Ding <chad_ding@126.com>
2017-05-30 23:46:15 +0800

    save

--- src/views/QueueForm/channelModal.js
+++ src/views/QueueForm/channelModal.js
@@ -6,53 +6,60 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Modal, Table, Input, Select, Button, Form } from 'antd';
+import { Modal, Table, Input, Select, Button, Form, Row, Col } from 'antd';
 import { handleModal, getSubscribeList } from './action';
 
 class Filter extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
-    render(){
+    render() {
         const { getFieldDecorator } = this.props.form;
         const FormItem = Form.Item;
         const formItemLayout = {
             labelCol: {
-                xs: { span: 12 },
-                sm: { span: 12 }
+                xs: { span: 5 },
+                sm: { span: 5 }
             },
             wrapperCol: {
-                xs: { span: 12 },
-                sm: { span: 12 }
+                xs: { span: 19 },
+                sm: { span: 19 }
             }
         };
 
         return (
-            <Form layout="inline" onSubmit={this.handleSubmit}>
-                <FormItem {...formItemLayout} label="集群">
-                    {getFieldDecorator('cluster', {
-                        initialValue: '1'
-                    })(
-                        <Select>
-                            <Option value="1">数据统计</Option>
-                            <Option value="2">数据同步</Option>
-                            <Option value="3">任务处理</Option>
-                            <Option value="4">事件广播</Option>
-                        </Select>
-                    )}
-                </FormItem>
-                <FormItem {...formItemLayout} label="关键字">
-                    {getFieldDecorator('keyword', {
-                        rules: [{
-                            type: 'string', message: '只能输入字符'
-                        }]
-                    })(
-                        <Input />
-                    )}
-                </FormItem>
-                <FormItem>
-                    <Button>搜索</Button>
-                </FormItem>
+            <Form onSubmit={this.handleSubmit}>
+                <Row gutter={40}>
+                    <Col span={10}>
+                        <FormItem {...formItemLayout} label="集群">
+                            {getFieldDecorator('cluster', {
+                                initialValue: '0'
+                            })(
+                                <Select>
+                                    <Option value="0">请选择</Option>
+                                    <Option value="1">数据统计</Option>
+                                    <Option value="2">数据同步</Option>
+                                    <Option value="3">任务处理</Option>
+                                    <Option value="4">事件广播</Option>
+                                </Select>
+                            )}
+                        </FormItem>
+                    </Col>
+                    <Col span={10}>
+                        <FormItem {...formItemLayout} label="关键字">
+                            {getFieldDecorator('keyword', {
+                                rules: [{
+                                    type: 'string', message: '只能输入字符'
+                                }]
+                            })(
+                                <Input />
+                            )}
+                        </FormItem>
+                    </Col>
+                    <Col span={4}>
+                        <Button size="large">搜索</Button>
+                    </Col>
+                </Row> 
             </Form>
         );
     }
@@ -65,15 +72,19 @@ class ChannelModal extends Component {
         super(props);
         this.handleCancel = this.handleCancel.bind(this);
         this.handleOk = this.handleOk.bind(this);
+        this.changePage = this.changePage.bind(this);
+    }
+    changePage(page, pageSize) {
+        console.log(page);
     }
     handleCancel() {
         const { dispatch } = this.props;
         dispatch(handleModal(false));
     }
-    componentWillReceiveProps(nextProps){
+    componentWillReceiveProps(nextProps) {
         const { dispatch } = this.props;
-        if(nextProps.channelModal.counter !== this.props.channelModal.counter && nextProps.channelModal.visible){
-           dispatch(getSubscribeList()); 
+        if (nextProps.channelModal.counter !== this.props.channelModal.counter && nextProps.channelModal.visible) {
+            dispatch(getSubscribeList());
         }
     }
     handleOk() {
@@ -98,11 +109,18 @@ class ChannelModal extends Component {
             key: 'status'
         }];
 
+        let pagination = {
+            current: 1,
+            onChange: this.changePage,
+            total: 5,
+            pageSize: 2
+        };
+
         return (
             <Modal width={800} maskClosable={false} onOk={this.handleOk} onCancel={this.handleCancel} title="订阅频道" key={this.props.channelModal.counter} visible={this.props.channelModal.visible}>
                 <FilterForm></FilterForm>
                 <br/>
-                <Table columns={columns} dataSource={this.props.subscribeList}></Table>
+                <Table pagination={pagination} columns={columns} dataSource={this.props.subscribeList}></Table>
             </Modal>
         );
     }
