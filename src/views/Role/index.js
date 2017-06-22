88af0919854ae595d40697c21bf8606be8d4c9b2
Chad.Ding <chad_ding@126.com>
2017-06-22 16:05:24 +0800

    role

--- src/views/Role/index.js
+++ src/views/Role/index.js
@@ -5,16 +5,95 @@
  */
 
 import React, { Component } from 'react';
+import { Form, Input, Button, Switch, Checkbox, Select, InputNumber, Breadcrumb, Icon, Table } from 'antd';
+import { connect } from 'react-redux';
 
-export default class Role extends Component {
+class RoleForm extends Component {
     constructor(props) {
         super(props);
     }
     render() {
+
+        let FormItem = Form.Item;
+        let { getFieldDecorator } = this.props.form;
+
+        let formItemLayout = {
+            labelCol: {
+                xs: { span: 14 },
+                sm: { span: 6 }
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 14 }
+            }
+        };
+
+        let tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0
+                },
+                sm: {
+                    span: 14,
+                    offset: 6
+                }
+            }
+        };
+
         return (
-            <div>
-                111111111111111
-            </div>
+            <Form onSubmit={this.handleSubmit}>
+                <FormItem {...formItemLayout} label="环境管理员" hasFeedback>
+                    {getFieldDecorator('envAdmin', {
+                        rules: [{
+                            type: 'string', message: '输入不合法!',
+                            pattern: /^queue.\w+/
+                        }, {
+                            required: true, message: '请输入频道名称!'
+                        }]
+                    })(
+                        <Input />
+                    )}
+                </FormItem>
+                <FormItem {...formItemLayout} label="资源审批员" hasFeedback>
+                    {getFieldDecorator('resourceAudit', {
+                        rules: [{
+                            type: 'string', message: '输入不合法!',
+                            pattern: /^queue.\w+/
+                        }, {
+                            required: true, message: '请输入频道名称!'
+                        }]
+                    })(
+                        <Input />
+                    )}
+                </FormItem>
+                <FormItem {...formItemLayout} label="系统管理员" hasFeedback>
+                    {getFieldDecorator('sysAdmin', {
+                        rules: [{
+                            type: 'string', message: '输入不合法!',
+                            pattern: /^queue.\w+/
+                        }, {
+                            required: true, message: '请输入频道名称!'
+                        }]
+                    })(
+                        <Input type="textarea" rows={5} />
+                    )}
+                </FormItem>
+                <FormItem {...tailFormItemLayout}>
+                    <Button type="primary" htmlType="submit" size="large">保存</Button>  
+                </FormItem>
+            </Form>
         );
     }
 };
+
+
+function mapStateToProps(state) {
+    return {
+
+    };
+}
+
+let Role = Form.create()(RoleForm);
+
+export default connect(mapStateToProps)(Role);
