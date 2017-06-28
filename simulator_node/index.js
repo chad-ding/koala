84dd2eed4f0ad068722fb5a70fc72d272949a62c
Chad.Ding <chad_ding@126.com>
2017-06-28 14:07:09 +0800

    remove useless code

--- simulator_node/index.js
+++ simulator_node/index.js
@@ -140,18 +140,5 @@ app.get('/api/chart/pie', function(req, res) {
     });
 });
 
-app.get('/api/chart/panel', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
-
-    fs.readFile(__dirname + '/data/panel_chart_data.json', { encoding: 'utf-8' }, function(err, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.end(data);
-    });
-});
-
 app.listen(8080);
-
 console.log('Server running on 8080');
