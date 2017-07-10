e1f4d8094d02c39db3afd83c4a90365f4de78676
Chad.Ding <chad_ding@126.com>
2017-07-10 17:31:58 +0800

    change

--- simulator_node/index.js
+++ simulator_node/index.js
@@ -8,149 +8,82 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
-app.get('/api/channel/list', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
+var options = [{
+    url: '/api/channel/list',
+    method: 'GET',
+    data: '/data/channel_list.json'
+}, {
+    url: '/api/queue/list',
+    method: 'GET',
+    data: '/data/queue_list.json'
+}, {
+    url: '/api/vdp/list',
+    method: 'GET',
+    data: '/data/vdp_list.json'
+}, {
+    url: '/api/subscribe/list',
+    method: 'GET',
+    data: '/data/subscribe_list.json'
+}, {
+    url: '/api/params/list',
+    method: 'GET',
+    data: '/data/params_list.json'
+}, {
+    url: '/api/domain/list',
+    method: 'GET',
+    data: '/data/domain_list.json'
+}, {
+    url: '/api/console/list',
+    method: 'GET',
+    data: '/data/console_list.json'
+}, {
+    url: '/api/cluster/list',
+    method: 'GET',
+    data: '/data/cluster_list.json'
+}, {
+    url: '/api/chart/line',
+    method: 'GET',
+    data: '/data/line_chart_data.json'
+}, {
+    url: '/api/chart/bar',
+    method: 'GET',
+    data: '/data/bar_chart_data.json'
+}, {
+    url: '/api/chart/pie',
+    method: 'GET',
+    data: '/data/pie_chart_data.json'
+}, {
+    url: '/api/chart/radar',
+    method: 'GET',
+    data: '/data/radar_chart_data.json'
+}];
 
-    fs.readFile(__dirname + '/data/channel_list.json', { encoding: 'utf-8' }, function(err, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.end(data);
+
+function getFunction(option) {
+    var url = option.url,
+        data = option.data;
+
+    app.get(url, function(req, res) {
+        res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
+
+        fs.readFile(__dirname + data, { encoding: 'utf-8' }, function(err, data) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            res.end(data);
+        });
     });
+}
+
+options.forEach(function(option) {
+
+    if ('GET' === option.method) {
+        getFunction(option);
+    }
+
 });
 
-app.get('/api/queue/list', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
-
-    fs.readFile(__dirname + '/data/queue_list.json', { encoding: 'utf-8' }, function(err, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.end(data);
-    });
-});
-
-app.get('/api/vdp/list', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
-
-    fs.readFile(__dirname + '/data/vdp_list.json', { encoding: 'utf-8' }, function(err, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.end(data);
-    });
-});
-
-app.get('/api/subscribe/list', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
-
-    fs.readFile(__dirname + '/data/subscribe_list.json', { encoding: 'utf-8' }, function(err, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.end(data);
-    });
-});
-
-app.get('/api/params/list', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
-
-    fs.readFile(__dirname + '/data/params_list.json', { encoding: 'utf-8' }, function(err, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.end(data);
-    });
-});
-
-app.get('/api/domain/list', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
-
-    fs.readFile(__dirname + '/data/domain_list.json', { encoding: 'utf-8' }, function(err, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.end(data);
-    });
-});
-
-app.get('/api/console/list', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
-
-    fs.readFile(__dirname + '/data/console_list.json', { encoding: 'utf-8' }, function(err, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.end(data);
-    });
-});
-
-app.get('/api/cluster/list', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
-
-    fs.readFile(__dirname + '/data/cluster_list.json', { encoding: 'utf-8' }, function(err, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.end(data);
-    });
-});
-
-app.get('/api/chart/line', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
-
-    fs.readFile(__dirname + '/data/line_chart_data.json', { encoding: 'utf-8' }, function(err, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.end(data);
-    });
-});
-
-app.get('/api/chart/bar', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
-
-    fs.readFile(__dirname + '/data/bar_chart_data.json', { encoding: 'utf-8' }, function(err, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.end(data);
-    });
-});
-
-app.get('/api/chart/pie', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
-
-    fs.readFile(__dirname + '/data/pie_chart_data.json', { encoding: 'utf-8' }, function(err, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.end(data);
-    });
-});
-
-app.get('/api/chart/radar', function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
-
-    fs.readFile(__dirname + '/data/radar_chart_data.json', { encoding: 'utf-8' }, function(err, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        res.end(data);
-    });
-});
 
 app.listen(8081);
 console.log('Server running on 8081');
