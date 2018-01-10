f2bd6e60a599cb571f3081c8b9369b0dc1fed63c
chad.ding <chad.ding@vipshop.com>
2018-01-10 19:58:52 +0800

    save

--- src/components/HoneycombChart/index.js
+++ src/components/HoneycombChart/index.js
@@ -1,12 +1,13 @@
 /**
  *@Author: chad.ding
- *@Date: 2017-12-25 10:33:12
+ *@Date: 2017-12-28 10:13:03
  */
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import echarts from 'echarts';
 import { Icon } from 'antd';
+import { isNull } from '../../commons/utils';
 
 export default class HoneycombChart extends Component {
     constructor(props) {
@@ -15,10 +16,10 @@ export default class HoneycombChart extends Component {
     static option = {
         grid: {
             show: false,
-            left: 20,
-            top: 20,
-            right: 20,
-            bottom: 20
+            left: 0,
+            top: 0,
+            right: 0,
+            bottom: 0
         },
         animationDurationUpdate: 1500,
         animationEasingUpdate: 'quinticInOut'
@@ -29,23 +30,25 @@ export default class HoneycombChart extends Component {
             return result;
         }
 
-        let cellNum = Math.floor((totalWidth - cellWidth) / (0.75 * cellWidth));
+        let cellNum = Math.floor((totalWidth - cellWidth) / (0.75 * cellWidth + 3));
 
         return result + cellNum;
 
     }
-    renderCell(cellNums) {
-        let dataLen = this.props.data.length;
-
+    renderCell(cellNums, dataList) {
+        let dataLen = dataList.length;
         let series = [];
+
+        const { padding, symbolRadius } = this.props.options;
+        let symbolHeight =  Math.sqrt(3) * symbolRadius;
 
         const baseOption = {
             type: 'graph',
             layout: 'none',
-            symbolSize: this.props.options.symbolSize,
-            width: this.props.options.symbolSize,
+            symbolSize: [symbolRadius * 2, symbolHeight],
+            width: symbolRadius * 2,
             symbol: 'image://' + require('../../../assets/img/hexagon.png'),
-            roam: true,
+            roam: false,
             label: {
                 normal: {
                     show: true
@@ -54,15 +57,21 @@ export default class HoneycombChart extends Component {
         };
 
         for (let i = 0; i < dataLen; i++) {
-            let data = this.props.data[i];
+            let data = dataList[i];
 
             let groupNum = Number.parseInt(i / cellNums);
             let index = i % cellNums;
             let offset = 3;
 
             let option = {
-                left: 0.75 * this.props.options.symbolSize * index + offset * index,
-                top: index % 2 ? this.props.options.symbolSize / 2 + groupNum * this.props.options.symbolSize + offset * groupNum : groupNum * this.props.options.symbolSize + offset * groupNum,
+                left: 0.75 * symbolRadius * 2 * index + offset * index + padding,
+                top: (function(){
+                    if(index % 2){
+                        return symbolHeight / 2 + groupNum * symbolHeight + offset * groupNum + padding;
+                    }else {
+                        return groupNum * symbolHeight + offset * groupNum + padding;
+                    }
+                })(),
                 data: [{
                     name: data,
                     x: 0,
@@ -73,19 +82,42 @@ export default class HoneycombChart extends Component {
         }
         return series;
     }
-    componentWillReceiveProps() {
-        let renderWidth = this.wrapDiv.clientWidth - this.constructor.option.grid.left - this.constructor.option.grid.right;
-        let symbolSize = this.props.options.symbolSize;
+    componentWillReceiveProps(nextProps) {
 
-        let cellNums = this.constructor.countGroup(renderWidth, symbolSize);
+        if(this.props.data !== nextProps.data){
+            this.renderChart(nextProps.data);
+        }
+        
+    }
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.resizeChart);
+    }
+    renderChart(data = this.props.data){
+        
+        this.chart.resize();
 
-        let series = this.renderCell(cellNums);
+        let { clientWidth} = this.wrapDiv;
+        const { symbolRadius, padding } = this.props.options;
+        let cellNums = this.constructor.countGroup(clientWidth - padding * 2, symbolRadius * 2);
+        
+        let series = this.renderCell(cellNums, data);
+        this.chart.setOption(Object.assign({ series }, this.constructor.option)); 
+    }
+    resizeChart() {
 
-        this.chart.setOption(Object.assign({ series }, this.constructor.option));
+        if (!isNull(this.timer)) {
+            window.clearTimeout(this.timer);
+        }
+
+        this.timer = window.setTimeout(() => {
+            this.renderChart();
+        }, 1000);
     }
     componentDidMount() {
-
         this.chart = echarts.init(this.wrapDiv);
+
+        this.resizeChart = this.resizeChart.bind(this);
+        window.addEventListener('resize', this.resizeChart);
     }
     render() {
         return (
@@ -101,8 +133,9 @@ HoneycombChart.propTypes = {
     title: PropTypes.string.isRequired,
     data: PropTypes.arrayOf(PropTypes.string).isRequired,
     options: PropTypes.shape({
-        symbolSize: PropTypes.number
-    }).isRequired
+        symbolRadius: PropTypes.number,
+        padding: PropTypes.number
+    })
 };
 
 HoneycombChart.defaultProps = {
@@ -112,6 +145,7 @@ HoneycombChart.defaultProps = {
         height: '400px'
     },
     options: {
-        symbolSize: 100
+        symbolRadius: 50,
+        padding: 20
     }
 };
